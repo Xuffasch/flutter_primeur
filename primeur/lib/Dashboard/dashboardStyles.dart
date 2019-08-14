0431933c91d662657a5a9fd14d6e2bba61167e66
Xuffasch <tristan.lucas@gmail.com>
2019-08-14 18:08:38 +0200

    Define ClientTitle and OrdersTitle styles

--- primeur/lib/Dashboard/dashboardStyles.dart
+++ primeur/lib/Dashboard/dashboardStyles.dart
@@ -2,9 +2,16 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 abstract class DashboardStyles {
-  static const TextStyle SectionTitle = TextStyle(
+  static const TextStyle ClientTitle = TextStyle(
     color: Colors.teal,
-    fontSize: 30,
+    fontSize: 28,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w800,
+  );
+
+  static const TextStyle OrderTitle = TextStyle(
+    color: Colors.orange,
+    fontSize: 28,
     fontStyle: FontStyle.normal,
     fontWeight: FontWeight.w800,
   );
