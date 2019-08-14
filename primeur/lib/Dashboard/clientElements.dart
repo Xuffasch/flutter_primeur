060ecdea7eb0ef6f921b1e7effe7767b6783cf46
Xuffasch <tristan.lucas@gmail.com>
2019-08-14 16:56:04 +0200

    Renames and updates titleCard to clientElements

---
+++ primeur/lib/Dashboard/clientElements.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+// Display the Client section title
+class ClientBanner extends StatelessWidget {
+  final title;
+
+  const ClientBanner({
+    this.title
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
+
+// Displau the Client section content
+class ClientContent extends StatelessWidget {
+  final name;
+  final address;
+
+  const ClientContent({
+    this.name,
+    this.address,
+  });
+
+  @override 
+  Widget build(BuildContext context) {
+
+  }
+}
+
