738db85d0a77b4e91005f2485f6b7cdadc55c839
Xuffasch <tristan.lucas@gmail.com>
2019-08-14 16:27:35 +0200

    Initializes a Dashboard widget folder

---
+++ primeur/lib/Dashboard/titleCard.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TitleCard extends StatelessWidget {
+  final title;
+
+  const TitleCard({
+    this.title
+  })
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
