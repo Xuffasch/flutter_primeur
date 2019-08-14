2dd3723475eb92a77ca57f5b68003f106953f22c
Xuffasch <tristan.lucas@gmail.com>
2019-08-14 18:16:59 +0200

    Create the orders Banner and Content elements

---
+++ primeur/lib/Dashboard/orderElements.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'dashboardStyles.dart';
+
+class OrderBanner extends StatelessWidget {
+  final title;
+
+  const OrderBanner({
+    this.title
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Text(title, style: DashboardStyles.OrderTitle),
+        ],
+      ),
+    );
+  }
+
+}
