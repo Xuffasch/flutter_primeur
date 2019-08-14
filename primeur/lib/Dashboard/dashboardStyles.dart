43acdb3be31953a2a7152da586a9f64f0889d216
Xuffasch <tristan.lucas@gmail.com>
2019-08-14 16:40:04 +0200

    Rename and move styles.dart in dashboardStyles

--- primeur/lib/Dashboard/dashboardStyles.dart
+++ primeur/lib/Dashboard/dashboardStyles.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
-abstract class Styles {
-  static const TextStyle boardSectionTitle = TextStyle(
+abstract class DashboardStyles {
+  static const TextStyle SectionTitle = TextStyle(
     color: Colors.teal,
     fontSize: 30,
     fontStyle: FontStyle.normal,
     fontWeight: FontWeight.w800,
   );
 
-  static const TextStyle boardSectionContent = TextStyle(
+  static const TextStyle SectionContent = TextStyle(
     fontSize: 16,
     fontStyle: FontStyle.normal,
     fontWeight: FontWeight.w400,
