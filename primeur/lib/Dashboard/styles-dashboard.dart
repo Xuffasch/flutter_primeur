95a829829df91bf73eafda8759fb8a19bb915191
Xuffasch <tristan.lucas@gmail.com>
2019-08-15 12:56:30 +0200

    Added the SectionLabel TextSyle

--- primeur/lib/Dashboard/styles-dashboard.dart
+++ primeur/lib/Dashboard/styles-dashboard.dart
@@ -2,9 +2,13 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 abstract class DashboardStyles {
+  static const double titleSize = 28;
+  static const double sectionLabelSize = 18;
+  static const double sectionContentSize = 16;
+
   static const TextStyle ClientTitle = TextStyle(
     color: Colors.teal,
-    fontSize: 28,
+    fontSize: titleSize,
     fontStyle: FontStyle.normal,
     fontWeight: FontWeight.w800,
   );
@@ -14,6 +18,12 @@ abstract class DashboardStyles {
     fontSize: 28,
     fontStyle: FontStyle.normal,
     fontWeight: FontWeight.w800,
+  );
+
+  static const TextStyle SectionLabel = TextStyle(
+    fontSize: sectionLabelSize,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w600,
   );
 
   static const TextStyle SectionContent = TextStyle(
