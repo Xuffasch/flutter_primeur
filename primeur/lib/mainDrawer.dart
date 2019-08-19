48f9ff5db81c463f3aef37cd6d65df70d4ae0225
Xuffasch <tristan.lucas@gmail.com>
2019-08-19 15:23:23 +0200

    Add Drawer widget for navigation

---
+++ primeur/lib/mainDrawer.dart
@@ -0,0 +1,99 @@
+import "package:flutter/material.dart";
+import "0-Dashboard/homePage.dart";
+import "1-Clients/clientListPage.dart";
+import "2a-Orders/orderListPage.dart";
+import "2b-Procurements/procurementListPage.dart";
+
+class MainDrawer extends StatelessWidget {
+  final accountName;
+  final sourceScreen;
+
+  String get accountAbbrv => accountName.substring(0, 2);
+
+  const MainDrawer({
+    this.accountName,
+    this.sourceScreen,
+  });
+
+  Widget nextWidget(String destination) {
+    switch (destination) {
+      case "Dashboard":
+        return HomePage(title: "Récapitulatif");
+      case "Client":
+        return ClientListPage(title: "Client");
+      case "Order":
+        return OrderListPage(title: "Commande");
+      case "Procurement":
+        return ProcurementListPage(title: "Achats");
+      default:
+        return HomePage(title: "Récapitulatif");
+    }
+  }
+
+  void transition(BuildContext context, String from, String to) {
+    if (from != to) {
+      print(to + " screen is called from " + from);
+      Navigator.of(context).pop();
+      Navigator.of(context).push(PageRouteBuilder(
+        pageBuilder: (BuildContext context, _, __) {
+          return nextWidget(to);
+        },
+        transitionsBuilder: (_, Animation<double> animation, __, Widget child) {
+          return FadeTransition(
+            opacity: animation,
+            child: child,
+          );
+        }, 
+      ));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text(accountName),
+            accountEmail: null,
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.yellow,
+              child: Text(
+                accountAbbrv,
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            ),
+          ),
+          ListTile(
+              leading: Text("Récapitulatif"),
+              onTap: () {
+                transition(context, sourceScreen, "Dashboard");
+              }),
+          Divider(),
+          ListTile(
+            leading: Text("Clients"),
+            onTap: () {
+              transition(context, sourceScreen, "Client");
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Text("Commandes"),
+              onTap: () {
+                transition(context, sourceScreen, "Order");
+            }
+          ),
+          Divider(),
+          ListTile(
+            leading: Text("Achats"),
+            onTap: () {
+              transition(context, sourceScreen, "Procurement");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
