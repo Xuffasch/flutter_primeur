8829d1927066ca8d869127593924d92fe0fcbc2f
Xuffasch <tristan.lucas@gmail.com>
2019-08-22 12:30:05 +0200

    Create the Client class to decode server data

---
+++ primeur/lib/1-Clients/client.dart
@@ -0,0 +1,28 @@
+class Client {
+  final int id;
+  final String business;
+  final String addressNumber;
+  final String address;
+  final int postcode;
+  final String city;
+
+  Client({
+    this.id,
+    this.business,
+    this.addressNumber,
+    this.address,
+    this.postcode,
+    this.city,
+  });
+
+  factory Client.fromJSON(Map<String, dynamic> json) {
+    return Client(
+      id: json['id'],
+      business: json['business'],
+      addressNumber: json['addressNumber'],
+      address: json['address'],
+      postcode: json['postcode'], 
+      city: json['city'],
+    );
+  }
+}
